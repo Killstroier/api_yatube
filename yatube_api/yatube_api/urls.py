80fde200b646086c471292e60e36d9f1f19b00bb
Aleksandr Matveev <aleksmatveev19@mail.ru>
2025-04-02 13:00:51 +0700

    дополнил в связи с изменениями

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,18 +1,29 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api/v1/jwt/create/',
+         TokenObtainPairView.as_view(),
+         name='token_obtain_pair'
+         ),
+    path('api/v1/jwt/refresh/',
+         TokenRefreshView.as_view(),
+         name='token_refresh'
+         ),
+    path('api/v1/jwt/verify/',
+         TokenVerifyView.as_view(),
+         name='token_verify'
+         ),
+    path('redoc/',
+         TemplateView.as_view(template_name='redoc.html'),
+         name='redoc'
+         ),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
-    urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    )
